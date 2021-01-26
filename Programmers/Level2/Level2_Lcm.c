79353853d4110c96f2c2d97e7e115c1fdade2034
leeki <eternityk0716@gmail.com>
2021-01-26 22:32:18 +0900

    Programmers_PS

---
+++ Programmers/Level2/Level2_Lcm.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// arr_len은 배열 arr의 길이입니다.
+int solution(int arr[], size_t arr_len) {
+    int answer = 0;
+    answer=arr[0];
+    for(int i=1; i<arr_len; i++)
+    {
+        answer=lcd(answer,arr[i]);
+    }
+    
+    return answer;
+}
+int gcd(int x, int y)
+{
+    if(x==0){
+        return y;
+    }else{
+        return gcd(y%x, x);
+    }
+}
+int lcm(int x, int y)
+{
+    return x*y/gcd(x, y);
+    
+}
